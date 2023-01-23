99ae24f5d1af325a5ad76432eb917ed5b58d05ba
Galact07 <sidharthnair1907@gmail.com>
2023-01-23 19:03:07 +0530

    commit

--- app.js
+++ app.js
@@ -1,75 +1,76 @@
-if(process.env.NODE_ENV!=="production"){
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express= require('express')
-const app= express()
-const path= require('path')
-const methodOverride= require('method-override')
-const ejsMate= require('ejs-mate')
-const mongoose= require('mongoose')
-const ExpressError= require('./utils/ExpressError')
-const campgroundsRoutes= require('./routes/campgrounds')
-const reviewsRoutes= require('./routes/reviews')
-const userRoutes= require('./routes/user')
-const session= require('express-session')
-const flash= require('connect-flash')
-const passport = require('passport')
-const LocalStrategy= require('passport-local')
-const User= require('./models/user')
-const mongoSanitize= require('express-mongo-sanitize')
-const MongoStore= require('connect-mongo')
-const helmet= require('helmet')
-const dbUrl=  process.env.DB_URL || 'mongodb://0.0.0.0:27017/yelp-camp'
-const secret= process.env.SECRET || 'itshouldbeagoodpass'
-main().catch(err => console.log(err));
-    // 
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const mongoose = require("mongoose");
+const ExpressError = require("./utils/ExpressError");
+const campgroundsRoutes = require("./routes/campgrounds");
+const reviewsRoutes = require("./routes/reviews");
+const userRoutes = require("./routes/user");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const mongoSanitize = require("express-mongo-sanitize");
+const MongoStore = require("connect-mongo");
+const helmet = require("helmet");
+const dbUrl = process.env.DB_URL || "mongodb://0.0.0.0:27017/yelp-camp";
+const secret = process.env.SECRET || "itshouldbeagoodpass";
+main().catch((err) => console.log(err));
+//
 async function main() {
-  await mongoose.connect(dbUrl)
-  console.log('mongodb connection established');
-}   
-app.engine('ejs',ejsMate)
+  await mongoose.connect(dbUrl);
+  console.log("mongodb connection established");
+}
+app.engine("ejs", ejsMate);
 
-app.set('view engine', 'ejs')
-app.set('views',path.join(__dirname,'views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended:true}))
-app.use(methodOverride('_method')) 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
-const store= MongoStore.create({
-    mongoUrl: dbUrl,
-    secret,
-    touchAfter: 24*60*60
-})
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  secret,
+  touchAfter: 24 * 60 * 60,
+});
 
-store.on("error",(e)=>{
-    console.log("Session Store Error",e)
-})
+store.on("error", (e) => {
+  console.log("Session Store Error", e);
+});
 
-app.use(session({
+app.use(
+  session({
     store,
-    name:'session',
+    name: "session",
     secret,
-    resave:false,
-    saveUninitialized:true,
-    cookie:{
-        httpOnly:true,
-        expires: Date.now() + 1000*60*60*24*7,
-        maxAge:1000*60*60*24*7
-    }
-})) 
-app.use(express.static(path.join(__dirname,'public')))
-app.use(mongoSanitize())
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize());
 // app.use(
 //   helmet({
 //     contentSecurityPolicy: false,
 //   })
 // );
 // app.use(helmet({contentSecurityPolicy:false}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(flash())
-
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 // const scriptSrcUrls = [
 //     "https://stackpath.bootstrapcdn.com",
@@ -107,7 +108,7 @@ app.use(flash())
 //                 "'self'",
 //                 "blob:",
 //                 "data:",
-//                 "https://res.cloudinary.com/dhhrw8dv4/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+//                 "https://res.cloudinary.com/dhhrw8dv4/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
 //                 "*",
 //             ],
 //             fontSrc: ["'self'", ...fontSrcUrls],
@@ -115,38 +116,34 @@ app.use(flash())
 //     })
 // );
 
-passport.use(new LocalStrategy(User.authenticate()))
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundsRoutes);
+app.use("/campgrounds/:id/reviews", reviewsRoutes);
 
-app.use((req,res,next) =>{
-    res.locals.currentUser= req.user
-    res.locals.success= req.flash('success')
-    res.locals.error= req.flash('error')
-    next()
-})
-app.use('/',userRoutes)
-app.use('/campgrounds',campgroundsRoutes)
-app.use('/campgrounds/:id/reviews',reviewsRoutes)
+const port = 9000;
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
+});
 
+app.get("/home", (req, res) => {
+  res.render("home.ejs");
+});
 
-const port= process.env.PORT || 9000
-app.listen(port,()=>{
-    console.log(`listening on http://localhost:${port}`);
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
 
-
-app.get('/home',(req,res)=>{
-    res.render('home.ejs')
-})
-
-app.all('*',(req,res,next)=>{
-    next(new ExpressError("Page Not Found",404))
-})
-
-app.use((err,req,res,next)=>{
-    const {message="Something went wrong",statusCode=500,stack} =err
-    res.status(statusCode).render('error.ejs',{message,stack})
-    
-})
+app.use((err, req, res, next) => {
+  const { message = "Something went wrong", statusCode = 500, stack } = err;
+  res.status(statusCode).render("error.ejs", { message, stack });
+});
